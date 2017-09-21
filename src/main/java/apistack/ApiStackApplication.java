04b940c8ccf1fbbb06f96ed2ed769daa02d7307c
jeskert <568361463@qq.com>
2017-09-21 11:51:25 +0800

    add some code and modify the pom.xml

---
+++ src/main/java/apistack/ApiStackApplication.java
@@ -0,0 +1,89 @@
+package apistack;
+
+import com.tw.apistack.config.Constants;
+import com.tw.apistack.endpoint.todo.dto.TodoDTO;
+import com.tw.apistack.repository.DummyTodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+@SpringBootApplication
+public class ApiStackApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ApiStackApplication.class);
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void initApplication() {
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+                && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+            LOG.error("You have misconfigured your application! It should not run "
+                    + "with both the 'dev' and 'prod' profiles at the same time.");
+        }
+    }
+
+    /**
+     * Main method, used to run the application.
+     *
+     * @param args the command line arguments
+     * @throws UnknownHostException if the local host name could not be resolved into an address
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(ApiStackApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        LOG.info("\n----------------------------------------------------------\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Local: \t\t{}://localhost:{}\n\t"
+                        + "External: \t{}://{}:{}\n\t"
+                        + "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
+    }
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_LOCAL)
+    public CommandLineRunner setup(DummyTodoRepository toDoRepository) {
+        return (args) -> {
+            toDoRepository.add(new TodoDTO("Remove unused imports", true));
+            toDoRepository.add(new TodoDTO("Clean the code", true));
+            toDoRepository.add(new TodoDTO("Build the artifacts", false));
+            toDoRepository.add(new TodoDTO("Deploy the jar file", true));
+            LOG.info("The sample data has been generated");
+        };
+    }
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_TEST)
+    public CommandLineRunner setupTestEnv(DummyTodoRepository toDoRepository) {
+        return (args) -> {
+            toDoRepository.add(new TodoDTO("test-A", false));
+            toDoRepository.add(new TodoDTO("test-B", false));
+            LOG.info("The test data has been generated");
+        };
+    }
+}
---
+++ src/main/java/apistack/repository/DummyTodoRepository.java
@@ -0,0 +1,44 @@
+package apistack.repository;
+
+import com.tw.apistack.endpoint.todo.dto.TodoDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by jxzhong on 2017/7/3.
+ */
+@Repository
+public class DummyTodoRepository {
+
+    private Set<TodoDTO> todos = new HashSet<>();
+
+    public DummyTodoRepository() {
+        this.todos = new HashSet<>();
+    }
+
+    public List<TodoDTO> getAll() {
+        return todos.stream().collect(Collectors.toList());
+    }
+
+    public Optional<TodoDTO> findById(long id) {
+        return todos.stream().filter(todo -> todo.getId() == id).findFirst();
+    }
+
+    public void delete(TodoDTO todo) {
+        todos.remove(todo);
+    }
+
+    public void add(TodoDTO todo) {
+        if (todo.getId() == 0) {
+            todo.setId(todos.size() + 1);
+            todo.setOrder(1);
+        }
+        todos.add(todo);
+    }
+
+}
